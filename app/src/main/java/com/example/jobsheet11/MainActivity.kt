b51f4e39f69de97dd78d55a04fc45117ddc96dd4
istiana zulfa <zulfangaluran@gmail.com>
2023-11-13 10:05:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jobsheet11/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.jobsheet11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val Intent = Intent(  this, SecondActivity::class.java)
+            startActivity(Intent)
+        }
+
+    }
+}
